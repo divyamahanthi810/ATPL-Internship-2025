f51d86302ff444a0c44a5024c83decc36c5bb69f
divyamahanthi810 <147492093+divyamahanthi810@users.noreply.github.com>
2025-07-09 21:34:18 +0530

    Js

--- Java script/even odd/even.js
+++
@@ -1,7 +0,0 @@
-let a=prompt("Please Enter a number");
-        if(a%2==0){
-            alert("Entered number "+a+" is even number");
-        }
-        else{
-            alert("Entered number "+a+" is odd number");
-        }
