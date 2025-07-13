af89a690ffe3bb7aabeed41984c47c3ede0a8331
divyamahanthi810 <147492093+divyamahanthi810@users.noreply.github.com>
2025-07-13 23:19:50 +0530

    Angular

---
+++ Angular/Angularproject/src/app/app-module.ts
@@ -0,0 +1,25 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { UserGreetingComponent } from './user-greeting.component/user-greeting.component';
+
+@NgModule({
+  declarations: [
+    App,
+    UserGreetingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
