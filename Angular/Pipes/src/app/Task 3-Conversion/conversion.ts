f4bd9efa034929e1d567b70572d23934aa705444
divyamahanthi810 <147492093+divyamahanthi810@users.noreply.github.com>
2025-07-21 17:27:14 +0530

    Pipes

---
+++ Angular/Pipes/src/app/Task 3-Conversion/conversion.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-conversion',
+  imports: [CommonModule],
+  templateUrl: './conversion.html',
+  styleUrl: './conversion.css'
+})
+export class Conversion {
+  Name:string='Divya';
+  Name1:string='sandhya bindu';
+  Name2:string='Monika';
+}
