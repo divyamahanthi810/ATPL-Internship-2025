f4754ee8bb85ec9c804130f47952d4569880e56c
divyamahanthi810 <147492093+divyamahanthi810@users.noreply.github.com>
2025-07-21 08:22:41 +0530

    Directives

--- Angular/Directives 1/tasks1/app.ts
+++ Angular/Directives 1/tasks1/app.ts
@@ -8,45 +8,42 @@ import { Component,ViewChild,HostListener ,ElementRef,signal } from '@angular/co
 })
 export class App {
   protected readonly title = signal('angularproject');
+  //TASK 3
   isDisabled = false;
   name = '';
   toggleDisable() {
     this.isDisabled = !this.isDisabled;
   }
-  userRole: 'admin' | 'user' = 'admin'; // simulate role
+  //TASK 4
+  userRole: 'admin' | 'user' = 'admin'; 
   toggleRole() {
     this.userRole = this.userRole === 'admin' ? 'user' : 'admin';
 }
 
+  //TASK 5
   elementWidth = 30;
   elementHeight = 40;
   boxColor = 'lightblue';
-
   @ViewChild('resizableDiv') resizableDiv!: ElementRef;
-
   ngAfterViewInit() {
     this.updateSize();
   }
-
   @HostListener('window:resize')
   onResize() {
     this.updateSize();
   }
-
   updateSize() {
     const width = this.resizableDiv.nativeElement.offsetWidth;
     const height = this.resizableDiv.nativeElement.offsetHeight;
-
     this.elementWidth = width;
     this.elementHeight = height;
-
     console.log(`Element size: ${width}px x ${height}px`);
-
-    // Update color using logic
     this.boxColor = width < 400 ? 'lightcoral' : 'lightgreen';
   }
-  countdown = 0;       // Countdown timer
-  isDisabld  = false;  // Button state
+
+  //TASK 6
+  countdown = 0;     
+  isDisabld  = false;
   originalText = 'Resend OTP';
   buttonText = this.originalText;
 
@@ -54,19 +51,17 @@ export class App {
     if (this.isDisabled) return;
 
     this.isDisabled = true;
-    this.countdown = 5; // seconds
+    this.countdown = 5;
     this.updateButtonText();
-
     const interval = setInterval(() => {
       this.countdown--;
       this.updateButtonText();
-
       if (this.countdown <= 0) {
         clearInterval(interval);
         this.isDisabled = false;
         this.buttonText = this.originalText;
       }
-    }, 500);
+    }, 1000);
   }
   updateButtonText() {
     this.buttonText = `${this.originalText} (${this.countdown})`;
