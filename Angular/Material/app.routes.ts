f5af870df127f2fad1230ca7ad7dcd8fd99b452a
divyamahanthi810 <147492093+divyamahanthi810@users.noreply.github.com>
2025-07-25 13:48:06 +0530

    Update app.routes.ts

--- Angular/Material/app.routes.ts
+++ Angular/Material/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Profile } from './profile/profile';
+import { Setting } from './setting/setting';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'dashboard',component:Dashboard},
+    {path:'profile',component:Profile},
+    {path:'setting',component:Setting},
+];
