d69181c7a87d2887306948470e1861c103d700e5
Mahanthi Divya <divyamahanthi810@gmail.com>
2025-08-11 20:12:56 +0530

    java3

--- Assignment3/StringSafe.java
+++ Assignment3/StringSafe.java
@@ -1,16 +1,20 @@
 import java.util.*;
 public class StringSafe {
-    public void Encrypt(String s1){
-        s1.concat(" World");
-        System.out.println(s1);
+    static String encryptpassword(String password){
+        StringBuffer sb=new StringBuffer();
+        for(char c:password.toCharArray())
+        {
+            sb.append((char) (c-2));
+        }
+        return sb.toString();
 
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Password");
         String s1=sc.nextLine();
-        StringSafe s=new StringSafe();
-        s.Encrypt("HI");
-        System.out.println(s1);
+        String Encrypt=encryptpassword(s1);
+        System.out.println("OriginalString "+s1);
+        System.out.println("EncryptedString "+Encrypt);
         }
     }
--- Assignment3/StringSorting.java
+++ Assignment3/StringSorting.java
@@ -1,11 +1,16 @@
-
+import java.util.Collections;
 import java.util.Arrays; 
 public class StringSorting {
     public static void main(String[] args) 
     {   
     String[] names = {"Divya","Anu","Priya","Geetika","Ramya"};
+    System.out.println("Array sorted in Ascending Order");
     Arrays.sort(names);
-    Arrays.sort(names).reverse();
+    for(String i:names){
+    System.out.println(i);
+    }
+    System.out.println("Array sorted in Descending Order");
+    Arrays.sort(names, Collections.reverseOrder());
     for(String i:names){
     System.out.println(i);
     }
